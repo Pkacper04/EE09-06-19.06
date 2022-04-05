a6c85488c2a1c5876ef0cedce79cecea218c3e53
Pkacper04 <kacperpotaczala001@gmail.com>
2022-04-05 10:58:16 +0200

    PHP DONE

--- zgloszenie.php
+++ zgloszenie.php
@@ -1,1 +1,18 @@
+<?php
+$NrZespolu = $_POST["NRRatowniczy"];
+$NrDyspozytora = $_POST["NRDyspozytora"];
+$Adres = $_POST["Adres"];
 
+$con = new mysqli("localhost","root","","ratownictwo");
+
+$date = date("h:i:s");
+$query = "INSERT INTO zgloszenia (ratownicy_id,dyspozytorzy_id,adres,pilne,czas_zgloszenia) VALUES ($NrZespolu,$NrDyspozytora,'$Adres',0,'$date'); ";
+
+
+$con->query($query);
+
+$con->close();
+
+include("pogotowie.html");
+
+?>
